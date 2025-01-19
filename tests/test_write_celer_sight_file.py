de1f2f6fbff045644a3ada0e5f6e44d384008d26
Manos Chaniotakis <manos.chaniotakis.n@gmail.com>
2025-01-19 17:53:16 +0200

    fix + expand test read write case

--- tests/test_write_celer_sight_file.py
+++ tests/test_write_celer_sight_file.py
@@ -2,6 +2,7 @@ import logging
 import os
 import unittest
 
+import numpy as np
 import pytest
 import qttest_utils
 from parameterized import parameterized
@@ -19,36 +20,46 @@ DELAY_TIME = 200  # in ms
 
 
 class TestCelerSightFile(BaseGuiTestCase, BaseImageTestCase):
-    def setUp(self):
-        # import celer_sight_ai main module
-        self.gui_main = qttest_utils.get_gui_main(offline=True)
 
     @pytest.mark.long
-    @parameterized.expand(BaseImageTestCase._load_mock_write_load_celer_sight_file())
-    def test_write_read_celer_sight_file(self, image_path):
-        self.write_celer_sight_file(image_path)
-        self.read_celer_sight_file()
+    def test_write_read_celer_sight_file(self):
+        for image_path, image_info in self.mock_all_images.items():
+            if not image_info.get("readable"):
+                continue
+            self.write_celer_sight_file((image_path,))
+            self.read_celer_sight_file()
 
     @pytest.mark.long
-    def write_celer_sight_file(self):
-        app = self.gui_main
-        qttest_utils.wait_until_shown(app.MainWindow)
-        qttest_utils.to_main_window(app)
+    def write_celer_sight_file(self, image_paths: tuple = ()):
+        import math
 
-        urls_to_be_added = [
-            "/Users/mchaniotakis/Downloads/Day 3 egglaying/combined/Day 3/aup-1_15_3.2023-12-14-13-47-12/aup-1_15_3_Bottom Right Dish_TM_p00_0_A01f00d0.TIF"
+        qttest_utils.to_main_window(self.app)
+
+        urls_to_be_added = image_paths
+        for image_path in urls_to_be_added:
+            assert os.path.exists(image_path), f"File does not exist: {image_path}"
+        QTest.qWait(DELAY_TIME * 10)
+
+        self.app.viewer.load_files_by_drag_and_drop(urls_to_be_added, auto_accept=True)
+        QTest.qWait(DELAY_TIME * 10)
+
+        current_image_uuid = self.app.DH.BLobj.get_current_image_uuid()
+        current_image_object = self.app.DH.BLobj.get_image_object_by_uuid(
+            current_image_uuid
+        )
+        current_image_size = current_image_object.SizeX, current_image_object.SizeY
+
+        center = [current_image_size[0] // 2, current_image_size[1] // 2]
+        # generate a polygon with 6 points in the center of the image
+        point_array = [
+            [
+                center[0] + 40 * math.cos(2 * math.pi * i / 6),
+                center[1] + 40 * math.sin(2 * math.pi * i / 6),
+            ]
+            for i in range(6)
         ]
-        assert os.path.exists(urls_to_be_added[0])
+        qttest_utils.draw_mask_by_array(self.app, np.array(point_array))
         QTest.qWait(DELAY_TIME * 10)
-
-        app.viewer.load_files_by_drag_and_drop(urls_to_be_added, auto_accept=True)
-        QTest.qWait(DELAY_TIME * 10)
-
-        app = qttest_utils.zoom_to_box(app, [12795, 9518, 12830, 9544])
-        QTest.qWait(DELAY_TIME * 20)
-
-        qttest_utils.magic_box_predict(app, [12795, 9518], [12830, 9544])
-        qttest_utils.close_notification_dialog()
 
         filename_save_location = os.path.join(config.APP_DATA_PATH, "temp")
         if not os.path.exists(filename_save_location):
@@ -57,20 +68,26 @@ class TestCelerSightFile(BaseGuiTestCase, BaseImageTestCase):
             filename_save_location, "test_file_celer_sight.bmics"
         )
 
-        groups_dict, filename = app.save_celer_sight_file(filename_save_location)
+        groups_dict, filename = self.app.save_celer_sight_file(filename_save_location)
+        qttest_utils.close_notification_dialog()
+        self.app.quit_project(without_prompt=True)
         return True
 
     @pytest.mark.long
     def read_celer_sight_file(self):
-        app = self.gui_main
-        qttest_utils.wait_until_shown(app.MainWindow)
-        qttest_utils.to_main_window(app)
+        qttest_utils.to_main_window(self.app)
 
         filename_save_location = os.path.join(
             config.APP_DATA_PATH, "temp", "test_file_celer_sight.bmics"
         )
-        app.load_celer_sight_file(filename_save_location)
-        QTest.qWait(DELAY_TIME * 100)
+        self.app.load_celer_sight_file(filename_save_location)
+        QTest.qWait(DELAY_TIME)
+        # make sure there is one image loaded
+        assert (
+            len(self.app.DH.BLobj.get_all_image_objects()) == 1
+        ), "There should be one image loaded"
+        self.app.quit_project(without_prompt=True)
+
         return True
 
 
